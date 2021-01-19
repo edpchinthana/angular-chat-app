bfa6c4e60ebf5a402977cdaf0f05f4455b38c3ea
edpchinthana <edpchinthana@outlook.com>
2021-01-19 23:14:48 +0530

    login and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
